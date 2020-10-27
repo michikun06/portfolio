da805adcca4ae3873cd7a0d1ca24882cf23d5130
kawaji michihiro <michikun06@icloud.com>
2020-10-27 20:54:24 +0900

    static削除

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
+// import { StaticQuery, graphql } from 'gatsby'
 
 import '../assets/scss/main.scss'
 
@@ -26,35 +26,34 @@ const Layout = ({ children, location }) => {
   }
 
   return (
-    <StaticQuery
-      query={graphql`
-        query SiteTitleQuery {
-          site {
-            siteMetadata {
-              title
-            }
-          }
-        }
-      `}
-      render={data => <div>{JSON.stringify(data)}</div>
-        //   (
-        // <>
-        //   <Helmet
-        //     title={data.site.siteMetadata.title}
-        //     meta={[
-        //       { name: 'description', content: 'Sample' },
-        //       { name: 'keywords', content: 'sample, something' },
-        //     ]}
-        //   >
-        //     <html lang="en" />
-        //   </Helmet>
-        //   {content}
-        // </>
-        // )
-      }
-    />
+    // <StaticQuery
+    //   query={graphql`
+    //     query SiteTitleQuery {
+    //       site {
+    //         siteMetadata {
+    //           title
+    //         }
+    //       }
+    //     }
+    //   `} data.site.siteMetadata.title
+    //   render={data => (
+    <>
+      <Helmet
+        title="Portfolio"
+        meta={[
+          { name: 'description', content: 'Sample' },
+          { name: 'keywords', content: 'sample, something' },
+        ]}
+      >
+        <html lang="en" />
+      </Helmet>
+      {content}
+    </>
   )
 }
+//     />
+//   )
+// }
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
