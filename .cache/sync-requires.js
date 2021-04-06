79308d22a925514df67f269b3019652b0461c5df
kawaji michihiro <michikun06@icloud.com>
2021-04-06 19:29:02 +0900

    写真追加

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/ipad/react/portfolio/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/ipad/react/portfolio/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/ipad/react/portfolio/src/pages/index.js"))),
   "component---src-pages-page-2-js": hot(preferDefault(require("/Users/ipad/react/portfolio/src/pages/page-2.js")))
